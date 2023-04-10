edc022a15c653eeab2fa4c159afc3c6c3b0a7866
2CHEVSKII <prsroman3@gmail.com>
2023-04-10 18:04:33 +0300

    init

---
+++ src/Waterfront.Server/Program.cs
@@ -0,0 +1,79 @@
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using Waterfront.AspNetCore.Extensions;
+using Waterfront.Core.Tokens.Signing.CertificateProviders;
+using Waterfront.Acl.Static;
+using Waterfront.Acl.Static.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables("WF_");
+
+string? customConfigFilePath = builder.Configuration.GetValue<string>("ConfigPath");
+
+if ( !string.IsNullOrEmpty(customConfigFilePath) )
+{
+    builder.Configuration.AddYamlFile(customConfigFilePath, true);
+}
+else
+{
+    builder.Configuration.AddYamlFile("wf_config.yaml", true);
+    builder.Configuration.AddYamlFile("wf_config.yml", true);
+    builder.Configuration.AddYamlFile("config.yaml", true);
+    builder.Configuration.AddYamlFile("config.yml", true);
+}
+
+builder.Host.UseSerilog(
+    (_, config) => config.WriteTo.Console(theme: AnsiConsoleTheme.Literate).MinimumLevel.Debug()
+);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddWaterfront(
+    wf => wf.ConfigureTokenOptions(builder.Configuration.GetSection("Tokens").Bind)
+            .ConfigureEndPoints(builder.Configuration.GetSection("Endpoints").Bind)
+            .WithCertificateProvider<FileSigningCertificateProvider,
+                FileTokenCertificateProviderOptions>(
+                opt => {
+                    opt.CertificatePath = "certs/localhost.crt";
+                    opt.PrivateKeyPath  = "certs/localhost.key";
+                }
+            )
+            .WithAuthentication<StaticAclAuthenticationService, StaticAclOptions>(
+                opt => {
+                    opt.Users = new[] {
+                        new StaticAclUser {
+                            Username = "localhostUser",
+                            Ip       = "127.0.0.1:*",
+                            Acl      = new[] { "localhost" }
+                        }
+                    };
+                    opt.Acl = new[] {
+                        new StaticAclPolicy {
+                            Name = "localhost",
+                            Access = new[] {
+                                new StaticAclPolicyAccessRule {
+                                    Type = "repository",
+                                    Name = "*",
+                                    Actions =
+                                    new[] { "pull", "push" }
+                                }
+                            }
+                        }
+                    };
+                }
+            )
+            .WithAuthorization<StaticAclAuthorizationService>()
+);
+
+var app = builder.Build();
+
+app.UseWaterfront();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
