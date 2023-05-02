4abf29d8dc4eca145a466430d246b73b43029eb2
2CHEVSKII <prsroman3@gmail.com>
2023-05-02 16:32:32 +0300

    configure configuration

--- src/Waterfront.Server/Program.cs
+++ src/Waterfront.Server/Program.cs
@@ -1,9 +1,11 @@
+using Dvchevskii.Extensions.Configuration.Yaml;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 using Waterfront.Acl.Static.Extensions.DependencyInjection;
 using Waterfront.Acl.Static.Models;
 using Waterfront.AspNetCore.Extensions;
 using Waterfront.Extensions.DependencyInjection;
+using YamlDotNet.Serialization.NamingConventions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -16,14 +18,14 @@ string? customConfigFilePath = builder.Configuration.GetValue(
 
 if ( !string.IsNullOrEmpty(customConfigFilePath) ) 
 {
-    builder.Configuration.AddYamlFile(customConfigFilePath, true);
+    builder.Configuration.AddYamlFile(customConfigFilePath, CamelCaseNamingConvention.Instance, false);
 }
 else
 {
-    builder.Configuration.AddYamlFile("wf_config.yaml", true);
-    builder.Configuration.AddYamlFile("wf_config.yml", true);
-    builder.Configuration.AddYamlFile("config.yaml", true);
-    builder.Configuration.AddYamlFile("config.yml", true);
+    builder.Configuration.AddYamlFile("wf_config.yaml", CamelCaseNamingConvention.Instance, true);
+    builder.Configuration.AddYamlFile("wf_config.yml", CamelCaseNamingConvention.Instance, true);
+    builder.Configuration.AddYamlFile("config.yaml", CamelCaseNamingConvention.Instance, true);
+    builder.Configuration.AddYamlFile("config.yml", CamelCaseNamingConvention.Instance, true);
 }
 
 builder.Host.UseSerilog(
@@ -34,50 +36,66 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddWaterfront(
-    wf => wf.AddTokenMiddleware()
-            .ConfigureTokens(tokens => tokens.SetIssuer("http://localhost:5050").SetLifetime(120))
-            .ConfigureEndpoints(opt => opt.SetTokenEndpoint("/token"))
-            .WithFileSigningCertificateProvider("./certs/localhost.crt", "./certs/localhost.key")
-            .WithDefaultTokenEncoder()
-            .WithDefaultTokenDefinitionService()
-            .AddStaticAuthentication(
-                new StaticAclUser {
-                    Username          = "root",
-                    PlainTextPassword = "rootpwd",
-                    Acl               = { "admin" }
-                },
-                new StaticAclUser {
-                    Username = "anonymous",
-                    Acl      = { "anonymous" }
-                }
-            )
-            .AddStaticAuthorization(
-                new StaticAclPolicy {
-                    Name = "anonymous",
-                    Access = {
-                        new StaticAclPolicyAccessRule {
-                            Type    = "repository",
-                            Name    = "**/*",
-                            Actions = { "pull" }
-                        }
-                    }
-                },
-                new StaticAclPolicy {
-                    Name = "admin",
-                    Access = {
-                        new StaticAclPolicyAccessRule {
-                            Type    = "registry",
-                            Name    = "**/*",
-                            Actions = { "*" }
-                        },
-                        new StaticAclPolicyAccessRule {
-                            Type    = "repository",
-                            Name    = "**/*",
-                            Actions = { "*" }
-                        }
-                    }
-                }
-            )
+    wf => {
+        wf.AddTokenMiddleware()
+          .ConfigureTokens(tokens => tokens.SetIssuer("http://localhost:5050").SetLifetime(120))
+          .ConfigureEndpoints(opt => opt.SetTokenEndpoint("/token"))
+          .WithFileSigningCertificateProvider("./certs/localhost.crt", "./certs/localhost.key")
+          .WithDefaultTokenEncoder()
+          .WithDefaultTokenDefinitionService()
+          /*.AddStaticAuthentication(
+              new StaticAclUser {
+                  Username          = "root",
+                  PlainTextPassword = "rootpwd",
+                  Acl               = { "admin" }
+              },
+              new StaticAclUser {
+                  Username = "anonymous",
+                  Acl      = { "anonymous" }
+              }
+          )
+          .AddStaticAuthorization(
+              new StaticAclPolicy {
+                  Name = "anonymous",
+                  Access = {
+                      new StaticAclAccessRule {
+                          Type    = "repository",
+                          Name    = "*#1#*",
+                          Actions = { "pull" }
+                      }
+                  }
+              },
+              new StaticAclPolicy {
+                  Name = "admin",
+                  Access = {
+                      new StaticAclAccessRule {
+                          Type    = "registry",
+                          Name    = "*#1#*",
+                          Actions = { "*" }
+                      },
+                      new StaticAclAccessRule {
+                          Type    = "repository",
+                          Name    = "*#1#*",
+                          Actions = { "*" }
+                      }
+                  }
+              }
+          )*/;
+
+        if ( builder.Configuration.GetSection("Users").Exists() )
+        {
+            wf.AddStaticAuthentication(
+                builder.Configuration.GetSection("Users").Get<StaticAclUser[]>()!
+            );
+        }
+
+        if ( builder.Configuration.GetSection("Acl").Exists() )
+        {
+            wf.AddStaticAuthorization(
+                builder.Configuration.GetSection("Acl").Get<StaticAclPolicy[]>()!
+            );
+        }
+    }
 );
 
 WebApplication app = builder.Build();
