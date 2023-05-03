7b143f2fb6e48ee201e315fba289a0f41fcf94fd
2CHEVSKII <prsroman3@gmail.com>
2023-05-03 07:43:02 +0300

    misc

--- src/Waterfront.Server/Program.cs
+++ src/Waterfront.Server/Program.cs
@@ -1,4 +1,5 @@
 using Dvchevskii.Extensions.Configuration.Yaml;
+using Dvchevskii.Extensions.Configuration.Yaml.FileExtensions;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 using Waterfront.Acl.Static.Extensions.DependencyInjection;
@@ -16,84 +17,45 @@ string? customConfigFilePath = builder.Configuration.GetValue(
     builder.Configuration.GetValue("Config_Path", string.Empty)!
 );
 
-if ( !string.IsNullOrEmpty(customConfigFilePath) ) 
+if (!string.IsNullOrEmpty(customConfigFilePath))
 {
-    builder.Configuration.AddYamlFile(customConfigFilePath, CamelCaseNamingConvention.Instance, false);
+    builder.Configuration.AddYamlFile(customConfigFilePath, CamelCaseNamingConvention.Instance);
 }
 else
 {
-    builder.Configuration.AddYamlFile("wf_config.yaml", CamelCaseNamingConvention.Instance, true);
+    builder.Configuration.AddYamlFile("wf_config.yaml", PascalCaseNamingConvention.Instance, true);
     builder.Configuration.AddYamlFile("wf_config.yml", CamelCaseNamingConvention.Instance, true);
     builder.Configuration.AddYamlFile("config.yaml", CamelCaseNamingConvention.Instance, true);
     builder.Configuration.AddYamlFile("config.yml", CamelCaseNamingConvention.Instance, true);
 }
 
-builder.Host.UseSerilog(
-    (_, config) => config.WriteTo.Console(theme: AnsiConsoleTheme.Literate).MinimumLevel.Debug()
-);
+builder.Host.UseSerilog((_, config) => config.WriteTo.Console(theme: AnsiConsoleTheme.Literate).MinimumLevel.Debug());
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddWaterfront(
-    wf => {
+    wf =>
+    {
         wf.AddTokenMiddleware()
-          .ConfigureTokens(tokens => tokens.SetIssuer("http://localhost:5050").SetLifetime(120))
+          .ConfigureTokens(
+              tokens =>
+              {
+                  builder.Configuration.GetSection("Tokens").Bind(tokens);
+              })
           .ConfigureEndpoints(opt => opt.SetTokenEndpoint("/token"))
           .WithFileSigningCertificateProvider("./certs/localhost.crt", "./certs/localhost.key")
           .WithDefaultTokenEncoder()
-          .WithDefaultTokenDefinitionService()
-          /*.AddStaticAuthentication(
-              new StaticAclUser {
-                  Username          = "root",
-                  PlainTextPassword = "rootpwd",
-                  Acl               = { "admin" }
-              },
-              new StaticAclUser {
-                  Username = "anonymous",
-                  Acl      = { "anonymous" }
-              }
-          )
-          .AddStaticAuthorization(
-              new StaticAclPolicy {
-                  Name = "anonymous",
-                  Access = {
-                      new StaticAclAccessRule {
-                          Type    = "repository",
-                          Name    = "*#1#*",
-                          Actions = { "pull" }
-                      }
-                  }
-              },
-              new StaticAclPolicy {
-                  Name = "admin",
-                  Access = {
-                      new StaticAclAccessRule {
-                          Type    = "registry",
-                          Name    = "*#1#*",
-                          Actions = { "*" }
-                      },
-                      new StaticAclAccessRule {
-                          Type    = "repository",
-                          Name    = "*#1#*",
-                          Actions = { "*" }
-                      }
-                  }
-              }
-          )*/;
+          .WithDefaultTokenDefinitionService();
 
-        if ( builder.Configuration.GetSection("Users").Exists() )
+        if (builder.Configuration.GetSection("Users").Exists())
         {
-            wf.AddStaticAuthentication(
-                builder.Configuration.GetSection("Users").Get<StaticAclUser[]>()!
-            );
+            wf.AddStaticAuthentication(builder.Configuration.GetSection("Users").Get<StaticAclUser[]>()!);
         }
 
-        if ( builder.Configuration.GetSection("Acl").Exists() )
+        if (builder.Configuration.GetSection("Acl").Exists())
         {
-            wf.AddStaticAuthorization(
-                builder.Configuration.GetSection("Acl").Get<StaticAclPolicy[]>()!
-            );
+            wf.AddStaticAuthorization(builder.Configuration.GetSection("Acl").Get<StaticAclPolicy[]>()!);
         }
     }
 );
