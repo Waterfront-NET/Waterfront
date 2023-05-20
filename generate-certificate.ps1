fdb01ac5f22bfa9cd0aee05ebb626d0482e3571d
2CHEVSKII <prsroman3@gmail.com>
2023-05-21 00:19:48 +0300

    Update certificate generation script

--- generate-certificate.ps1
+++ generate-certificate.ps1
@@ -1,3 +1,6 @@
+using namespace System
+using namespace System.IO
+
 [CmdletBinding()]
 param(
   $CertificatePath = 'localhost.crt',
@@ -11,5 +14,19 @@ param(
   $CN,
   $Email
 )
+
+$CertificatePath = [Path]::GetFullPath($CertificatePath, $PSScriptRoot)
+$PrivateKeyPath = [Path]::GetFullPath($PrivateKeyPath, $PSScriptRoot)
+
+$cert_path_directory = [Path]::GetDirectoryName($CertificatePath)
+$key_path_directory = [Path]::GetDirectoryName($PrivateKeyPath)
+
+if (-not [Directory]::Exists($cert_path_directory)) {
+  [Directory]::CreateDirectory($cert_path_directory)
+}
+
+if (-not [Directory]::Exists($key_path_directory)) {
+  [Directory]::CreateDirectory($key_path_directory)
+}
 
 "$Country`n$State`n$Locality`n$Organization`n$OrganizationUnit`n$CN`n$Email" | openssl req -x509 -new -nodes -out "$CertificatePath" -keyout "$PrivateKeyPath" -days $Days
